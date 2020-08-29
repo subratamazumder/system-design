b5191c7388fef369584b9cc180099bbada9388c9
Subrata Mazumder <subrata.besu@gmail.com>
2020-08-29 11:28:59 +0530

    server code added, ream me update

--- netwrok-protocols/README.md
+++ netwrok-protocols/README.md
@@ -1,3 +1,14 @@
 # Network Protocols
 ## Key Concepts
+### IP
+### TCP
+### HTTP
 ## Hands On
+### Set Up
+```
+npm install express
+```
+### Run
+```
+curl http://localhost:3000/
+```
---
+++ push-quick.sh
@@ -0,0 +1,15 @@
+if [ $# -eq 0 ]
+  then
+    echo "No commit message supplied, setting default"
+    export COMMIT_MESSAGE="minor update"
+fi
+if [ $# -eq 1 ]
+  then
+    export COMMIT_MESSAGE=$1
+fi
+
+echo "Committing with message as- "$COMMIT_MESSAGE
+git pull
+git add .
+git commit -m "$COMMIT_MESSAGE" .
+git push
